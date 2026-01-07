922b970bc46d211c8f99c924c0d92b05024ef032
drawinpay <drawinpay@qq.com>
2026-01-07 10:04:02 +0800

    添加字符串转换工具类

---
+++ src/Vinpay.Utils/StringUtil.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Vinpay.Utils
+{
+    public static class StringUtil
+    {
+        /// <summary>
+        /// Convert the byte array to a hexadecimal string
+        /// </summary>
+        /// <param name="data">The input byte array.</param>
+        /// <param name="separator">The separator between each byte.</param>
+        /// <param name="isLowerCase">Indicates whether the MD5 string is in lowercase</param>
+        /// <returns></returns>
+        public static string BytesToHexString(this byte[] data, string separator = "", bool isLowerCase = true)
+        {
+            StringBuilder sb = new StringBuilder();
+            string format = isLowerCase ? "x2" : "X2";
+            bool useSeparator = !string.IsNullOrEmpty(separator);
+
+            if (useSeparator)
+            {
+                for (int i = 0; i < data.Length - 1; i++)
+                {
+                    sb.Append(data[i].ToString(format));
+                    sb.Append(separator);
+                }
+                sb.Append(data[data.Length - 1].ToString(format));
+            }
+            else
+            {
+                for (int i = 0; i < data.Length; i++)
+                {
+
+                    sb.Append(data[i].ToString(format));
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}

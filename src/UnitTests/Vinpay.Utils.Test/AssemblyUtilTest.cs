000b0580791befd7189766b357e3d48c082342b7
drawinpay <drawinpay@qq.com>
2026-01-15 16:05:53 +0800

    Add a assembly util class and its unit test.

---
+++ src/UnitTests/Vinpay.Utils.Test/AssemblyUtilTest.cs
@@ -0,0 +1,37 @@
+using System.Reflection;
+
+namespace Vinpay.Utils.Test;
+
+[TestClass]
+public class AssemblyUtilTest
+{
+    [TestMethod]
+    public void GetAssemblyDirTest()
+    {
+        string expextedResult = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+        string assemblyDir = AssemblyUtil.GetAssemblyDir(Assembly.GetExecutingAssembly());
+
+        Assert.IsNotNull(assemblyDir);
+        Assert.AreEqual(expextedResult, assemblyDir);
+    }
+
+    [TestMethod]
+    public void GetCurrentAssemblyDirTest()
+    {
+        string assemblyDir = AssemblyUtil.GetCurrentAssemblyDir();
+        string expextedResult = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+
+        Assert.IsNotNull(assemblyDir);
+        Assert.AreEqual(expextedResult, assemblyDir);
+    }
+
+    [TestMethod]
+    public void GetEntryDirTest()
+    {
+        string expectedResult = Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!;
+        string entryDir = AssemblyUtil.GetEntryDir();
+
+        Assert.IsNotNull(entryDir);
+        Assert.AreEqual(expectedResult, entryDir);
+    }
+}
